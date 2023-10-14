20f1ccb9321e435296d9907315d0814b7e67ce33
undercurre <undercurre@163.com>
2023-10-15 05:59:57 +0800

    feat: 文档更新

--- assets/component_slider.md.41a07335.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o}from"./app.59ba4b6e.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"component/slider.md"}'),r={name:"component/slider.md"};function a(n,s,c,p,d,i){return t(),o("div")}const l=e(r,[["render",a]]);export{m as __pageData,l as default};
